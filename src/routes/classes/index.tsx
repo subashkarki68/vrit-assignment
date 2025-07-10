414548243b802d3de7e06266c83ae76f145013cd
ruchirajkarki <ruchirajkarki@gmail.com>
2025-07-10 10:36:58 +0545

    feat: enhance classes route with detailed course information and updated styles

--- src/routes/classes/index.tsx
+++ src/routes/classes/index.tsx
@@ -5,5 +5,40 @@ export const Route = createFileRoute('/classes/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/classes/"!</div>
+  return (
+    <div className="flex flex-col pt-20 px-10 gap-8">
+      <div className="flex flex-col gap-4">
+        <p className="text-xl">
+          Explore our classes and master trending skills!
+        </p>
+        <p className="font-bold text-4xl">
+          Dive Into{' '}
+          <span className="text-primary">What&apos;s Hot Right Now! 🔥</span>
+        </p>
+      </div>
+      <div className="flex flex-row gap-8 cursor-pointer">
+        <div className="bg-secondary-foreground rounded-4xl px-6 py-8 w-fit">
+          <div className="[writing-mode:sideways-lr] text-secondary h-70 p-8">
+            <h2 className="font-bold text-3xl">All Courses</h2>
+            <p>Courses you&apos;re powering through right now.</p>
+          </div>
+          <p className="text-secondary font-extrabold text-8xl">23+</p>
+        </div>
+        <div className="bg-secondary-foreground rounded-4xl px-6 py-8 w-fit">
+          <div className="[writing-mode:sideways-lr] text-secondary h-70 p-8">
+            <h2 className="font-bold text-3xl">All Courses</h2>
+            <p>Courses you&apos;re powering through right now.</p>
+          </div>
+          <p className="text-secondary font-extrabold text-8xl">23+</p>
+        </div>
+        <div className="bg-secondary-foreground rounded-4xl px-6 py-8 w-fit">
+          <div className="[writing-mode:sideways-lr] text-secondary h-70 p-8">
+            <h2 className="font-bold text-3xl">All Courses</h2>
+            <p>Courses you&apos;re powering through right now.</p>
+          </div>
+          <p className="text-secondary font-extrabold text-8xl">23+</p>
+        </div>
+      </div>
+    </div>
+  )
 }
