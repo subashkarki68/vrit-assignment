1bef71deb67f053b68db4ecf48faa67947206867
ruchirajkarki <ruchirajkarki@gmail.com>
2025-07-10 09:48:17 +0545

    refactor: simplify className usage in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,39 @@
 import { Link } from '@tanstack/react-router'
 
 export default function Header() {
+  const className =
+    'text-lg font-semibold text-gray-800 hover:text-blue-600 transition-colors'
+  const activeClassName = 'text-blue-600 border-b-2 border-blue-600'
   return (
     <header className="p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
         <div className="px-2 font-bold flex flex-row gap-4">
-          <Link 
+          <Link
             to="/"
-            className="hover:text-blue-600 transition-colors"
+            className={className}
             activeProps={{
-              className: "text-blue-600 border-b-2 border-blue-600"
+              className: activeClassName,
             }}
           >
             Home
           </Link>
-          <Link 
+          <Link
             to="/testimonials"
-            className="hover:text-blue-600 transition-colors"
+            className={className}
             activeProps={{
-              className: "text-blue-600 border-b-2 border-blue-600"
+              className: activeClassName,
             }}
           >
             Testimonials
+          </Link>
+          <Link
+            to="/classes"
+            className={className}
+            activeProps={{
+              className: activeClassName,
+            }}
+          >
+            Classes
           </Link>
         </div>
       </nav>
